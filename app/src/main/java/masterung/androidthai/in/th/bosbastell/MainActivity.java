5c7bf642aaa8b1f8561606555a164efa58eccf18
masterUNG <phrombutr@gmail.com>
2018-11-06 13:03:10 +0700

    Check Alert Success

--- app/src/main/java/masterung/androidthai/in/th/bosbastell/MainActivity.java
+++ app/src/main/java/masterung/androidthai/in/th/bosbastell/MainActivity.java
@@ -3,6 +3,7 @@ package masterung.androidthai.in.th.bosbastell;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -11,13 +12,15 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView tempTextView, humidTextView;
-    private String currentTempString, currentHumidString,
+    private String currentTempString = "20", currentHumidString = "30",
             alertTempString, alertHumidString;
+    private int alertTempAnInt = 30, alertHumidAnInt = 60;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,26 +41,85 @@ public class MainActivity extends AppCompatActivity {
         FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
         DatabaseReference databaseReference = firebaseDatabase.getReference();
 
-        databaseReference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+        try {
 
-                Map map = (Map) dataSnapshot.getValue();
-                currentTempString = String.valueOf(map.get("Temp"));
-                currentHumidString = String.valueOf(map.get("Humidity"));
+            databaseReference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-                tempTextView.setText(currentTempString);
-                humidTextView.setText(currentHumidString);
+                    Map map = (Map) dataSnapshot.getValue();
+                    currentTempString = String.valueOf(map.get("Temp"));
+                    currentHumidString = String.valueOf(map.get("Humidity"));
 
-            }
+                    tempTextView.setText(currentTempString);
+                    humidTextView.setText(currentHumidString);
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
+                    //                Check Temp
+                    if (findInt(currentTempString) > alertTempAnInt) {
+                        setupAlert(true);
+                    }
 
-            }
-        });
+                    //                Check Humid
+                    if (findInt(currentHumidString) > alertHumidAnInt) {
+                        setupAlert(false);
+                    }
+
+
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
 
     }   // realTime
+
+    private int findInt(String currentString) {
+
+        try {
+
+            return Integer.valueOf(currentString);
+
+        } catch (Exception e) {
+            return 0;
+        }
+
+
+
+    }
+
+    private void setupAlert(boolean status) {
+
+        Log.d("6NovV1", "status ==> " + status);
+
+
+
+        Map<String, Object> objectMap = new HashMap<>();
+
+        if (status) {
+//            Alert Temp
+            FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+            DatabaseReference databaseReference = firebaseDatabase.getReference().child("MyTemp");
+            objectMap.put("alertTemp", 1);
+            databaseReference.setValue(objectMap);
+
+        } else {
+//            Alert Humid
+            FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+            DatabaseReference databaseReference = firebaseDatabase.getReference().child("MyHumid");
+            objectMap.put("alertHumid", 1);
+            databaseReference.setValue(objectMap);
+        }   // if
+
+
+
+    }   // setup
 
 }   // Main Class
